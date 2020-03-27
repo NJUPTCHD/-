68260f2ecf66eba0189aa6ad7fd0d1bc812ccce3
NJUPTCHD <1904288508@qq.com>
2020-03-27 21:46:12 +0800

    更改数据库字段&实现用session筛选和判断

---
+++ add/add/settings.py
@@ -0,0 +1,115 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'vh#kpfoeq9rz&)yha1l0h=_b$sz1l79qww_lj2%)t8v9zn#@wg'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'retailer',
+    'welcome',
+    'other',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'add.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'add.wsgi.application'
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'combine',
+        'USER':'chd',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
+        'PORT':3306,
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'zh-hans'
+
+TIME_ZONE = 'Asia/Shanghai'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    ("js",os.path.join(STATIC_URL,'js')),
+    ("css",os.path.join(STATIC_URL,'css')),
+    os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+]
+
+MEDIA_KEY_PREFIX = "static/uploads"
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/uploads')
+
+INTERNAL_IPS = ('127.0.0.1','localhost','10.0.122.179')
+
+EMAIL_HOST = ""
+EMAIL_POST = 25
+EMAIL_HOST_USER = '我的邮箱@163.com'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
